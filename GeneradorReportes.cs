1654d0488ee5ba64287fda91d92d5d167d9a4b1e
MICA1221 <maricastrillon111@soy.sena.edu.co>
2023-04-28 09:01:21 -0500

    Primer commit del proyecto de reportes en Excel

---
+++ GeneradorReportes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using ClosedXML.Excel;
+
+namespace ReportesExcel
+{
+    public class GeneradorReportes
+    {
+        public void ExportarReporteAExcel(List<Producto> productos, string rutaArchivo)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Reporte de Productos");
+
+               
+                worksheet.Cell(1, 1).Value = "ID";
+                worksheet.Cell(1, 2).Value = "Nombre";
+                worksheet.Cell(1, 3).Value = "Precio";
+                worksheet.Cell(1, 4).Value = "Cantidad";
+
+                for (int i = 0; i < productos.Count; i++)
+                {
+                    var producto = productos[i];
+                    worksheet.Cell(i + 2, 1).Value = producto.Id;
+                    worksheet.Cell(i + 2, 2).Value = producto.Nombre;
+                    worksheet.Cell(i + 2, 3).Value = producto.Precio;
+                    worksheet.Cell(i + 2, 4).Value = producto.Cantidad;
+                }
+
+                workbook.SaveAs(rutaArchivo);
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReportesExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<Producto> productos = new List<Producto>
+            {
+                new Producto { Id = 1, Nombre = "Producto 1", Precio = 10.5m, Cantidad = 5 },
+                new Producto { Id = 2, Nombre = "Producto 2", Precio = 15.25m, Cantidad = 3 },
+                new Producto { Id = 3, Nombre = "Producto 3", Precio = 8.0m, Cantidad = 10 },
+                new Producto { Id = 4, Nombre = "Producto 4", Precio = 20.0m, Cantidad = 1 },
+            };
+
+          
+            var generadorReportes = new GeneradorReportes();
+            string rutaArchivo = "ReporteProductos.xlsx";
+            generadorReportes.ExportarReporteAExcel(productos, rutaArchivo);
+
+            Console.WriteLine($"El reporte ha sido generado y guardado en {rutaArchivo}");
+            Console.ReadKey();
+        }
+    }
+}
+
